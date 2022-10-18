3c7f235433770cec8b875bdb21095c3d052ea36d
MadokaN617 <114047971+MadokaN617@users.noreply.github.com>
2022-10-17 19:58:34 -0700

    課題提出テスト
    データ型、文字列クラス

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,21 @@
+public class Task1_1 {
+    public static void main(String[] args) {
+
+        int hogeInt = 50;
+        
+        String hogeString = "テスト";
+
+        //double型を宣言して、変数名はhogedoubleで 0.2で初期化
+        double hogeDouble = 0.2;
+
+        hogeInt = 100;
+        System.out.println(hogeInt);
+
+        hogeString="合格";
+        System.out.println(hogeString);
+
+        //hogeStringを表示
+
+        System.out.println(hogeDouble);
+    }   
+}
